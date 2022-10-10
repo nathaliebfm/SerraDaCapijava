96fa09143417d2db7c9a375b20ce505d38002143
Dry-Audrey <112403510+Dry-A@users.noreply.github.com>
2022-10-10 11:01:26 -0300

    Atualização camada Controller e Repository

--- projetointegrador/src/main/java/com/capijava/capijava/model/ProdutosModel.java
+++ projetointegrador/src/main/java/com/capijava/capijava/model/ProdutosModel.java
@@ -4,10 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_produtos")
@@ -45,6 +48,12 @@ public class ProdutosModel {
 
 	//Foto
 	private String foto; //Sem especificações 
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private CategoriaModel categoria;
+		
+	
 
 	public Long getId() {
 		return id;
@@ -101,4 +110,16 @@ public class ProdutosModel {
 	public void setFoto(String foto) {
 		this.foto = foto;
 	}
+
+	public CategoriaModel getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(CategoriaModel categoria) {
+		this.categoria = categoria;
+	}
+
+	
+	
+	
 }
